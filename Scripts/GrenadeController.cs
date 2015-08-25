7a44c320586e22386361c9be51dbcb0c913e0a61
Dane Johnson <dane_johnson@ymail.com>
2015-08-24 23:08:37 -0500

    Grenade now blows back enemies, sound file added

---
+++ Scripts/GrenadeController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GrenadeController : MonoBehaviour {
+
+	public float explosionRadius;
+	public float detonationMagnitude;
+
+	private float startTime;
+
+	// Use this for initialization
+	void Start () {
+
+		startTime = Time.time;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time - startTime > 5) {
+			Explode ();
+		}
+	}
+
+	private void Explode(){
+		GameObject[] baddies = GameObject.FindGameObjectsWithTag ("Baddie");
+
+		foreach (GameObject baddie in baddies) {
+			float magnitude = Vector2.Distance(transform.position, baddie.transform.position);
+
+			if(magnitude < explosionRadius){
+				baddie.GetComponent<Rigidbody2D>().AddForce((baddie.transform.position - transform.position).normalized * ((explosionRadius - magnitude) / explosionRadius) * detonationMagnitude);
+			}
+		}
+		AudioSource sound = GetComponent<AudioSource> ();
+		sound.Play ();
+		Destroy (gameObject);
+	}
+}
--- Scripts/PlayerController.cs
+++ Scripts/PlayerController.cs
@@ -4,10 +4,21 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
 	public float speed;
+	public GameObject grenadePrefab;
 
 	void FixedUpdate(){
 		Rigidbody2D rb = GetComponent<Rigidbody2D> ();
 		rb.AddTorque(Input.GetAxis("Horizontal") * -speed);
+	}
 
+	void Update(){
+		if (Input.GetButtonDown ("Fire1")) {
+			DropGrenade ();
+		}
+	}
+
+	private void DropGrenade(){
+		GameObject myGrenade = Instantiate (grenadePrefab);
+		myGrenade.transform.position = transform.position;
 	}
 }
