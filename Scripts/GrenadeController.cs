31606740ad92b1599217b2abd14ea3ba332234c5
Dane Johnson <dane_johnson@ymail.com>
2015-08-28 13:35:41 -0500

    Made additional changes to the grenade

--- Scripts/GrenadeController.cs
+++ Scripts/GrenadeController.cs
@@ -7,33 +7,47 @@ public class GrenadeController : MonoBehaviour {
 	public float detonationMagnitude;
 
 	private float startTime;
+	private bool isExploding;
+	private AudioSource sound;
 
 	// Use this for initialization
 	void Start () {
-
 		startTime = Time.time;
-	
+		sound = GetComponent<AudioSource> ();
+		isExploding = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Time.time - startTime > 5) {
+		if (Time.time - startTime > 5 && !isExploding) {
 			Explode ();
+		}
+	}
+
+	void FixedUpdate(){
+		if(isExploding && !sound.isPlaying){
+			Destroy(gameObject);
 		}
 	}
 
 	private void Explode(){
 		GameObject[] baddies = GameObject.FindGameObjectsWithTag ("Baddie");
+		GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+		GameObject[] grenades = GameObject.FindGameObjectsWithTag("Grenade");
 
-		foreach (GameObject baddie in baddies) {
-			float magnitude = Vector2.Distance(transform.position, baddie.transform.position);
+		GameObject[] items = Utilities.CombineGameObjectArrays(baddies, players, grenades);
 
-			if(magnitude < explosionRadius){
-				baddie.GetComponent<Rigidbody2D>().AddForce((baddie.transform.position - transform.position).normalized * ((explosionRadius - magnitude) / explosionRadius) * detonationMagnitude);
+		foreach (GameObject item in items) {
+			float magnitude = Vector2.Distance(transform.position, item.transform.position);
+
+			if(item != gameObject && magnitude < explosionRadius){
+				item.GetComponent<Rigidbody2D>().AddForce((item.transform.position - transform.position).normalized * ((explosionRadius - magnitude) / explosionRadius) * detonationMagnitude);
 			}
 		}
-		AudioSource sound = GetComponent<AudioSource> ();
 		sound.Play ();
-		Destroy (gameObject);
+		transform.rotation = Quaternion.identity;
+		GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
+		GetComponent<Animator>().enabled = true;
+		isExploding = true;
 	}
 }
---
+++ Scripts/Utilities.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Utilities {
+
+	public static GameObject[] CombineGameObjectArrays(params GameObject[][] arrays){
+		int totalSize = 0;
+		int overallIndex = 0;
+		
+		for(int i = 0; i < arrays.Length; i++){
+			totalSize += arrays[i].Length;
+		}
+		
+		GameObject[] combinedArrays = new GameObject[totalSize];
+		
+		for(int i = 0; i < arrays.Length; i++){
+			for(int j = 0; j < arrays[i].Length; j++){
+				combinedArrays[overallIndex] = arrays[i][j];
+				overallIndex++;
+			}
+		}
+		
+		return combinedArrays;
+	}
+}
