5b468bd7df93904c2e10e14e677917252daa5d75
Dane Johnson <dane_johnson@ymail.com>
2015-08-31 20:41:18 -0500

    Attached a script to the camera

---
+++ Scripts/CameraController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+	public float bufferX;
+	public float bufferY;
+
+	void Update(){
+		//Make a box, and follow if outside the box
+		if(player.transform.position.x > transform.position.x + bufferX){
+			transform.position = new Vector3(player.transform.position.x - bufferX,
+			                                 transform.position.y,
+			                                 transform.position.z);
+		} else if(player.transform.position.x < transform.position.x - bufferX){
+			transform.position = new Vector3(player.transform.position.x + bufferX,
+			                                 transform.position.y,
+			                                 transform.position.z);
+		}
+		if(player.transform.position.y > transform.position.y + bufferY){
+			transform.position = new Vector3(transform.position.x,
+			                                 player.transform.position.y - bufferY,
+			                                 transform.position.z);
+		} else if(player.transform.position.y < transform.position.y - bufferY){
+			transform.position = new Vector3(transform.position.x,
+			                                 player.transform.position.y + bufferY,
+			                                 transform.position.z);
+		}
+	}
+}
--- Scripts/PlayerController.cs
+++ Scripts/PlayerController.cs
@@ -6,14 +6,21 @@ public class PlayerController : MonoBehaviour {
 	public float speed;
 	public GameObject grenadePrefab;
 
+	private float lastGrenadeTime;
+
+	void Start(){
+		lastGrenadeTime = Time.time - 3.0f;
+	}
+
 	void FixedUpdate(){
 		Rigidbody2D rb = GetComponent<Rigidbody2D> ();
 		rb.AddTorque(Input.GetAxis("Horizontal") * -speed);
 	}
 
 	void Update(){
-		if (Input.GetButtonDown ("Fire1")) {
+		if (Input.GetButtonDown ("Fire1") && Time.time - lastGrenadeTime > 3.0f) {
 			DropGrenade ();
+			lastGrenadeTime = Time.time;
 		}
 	}
 
