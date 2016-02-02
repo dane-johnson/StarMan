d2405ceea1e5fc0467e5f825d5f4cb7fef962173
Dane Johnson <dane_johnson@ymail.com>
2016-02-01 20:48:17 -0600

    Added background, navigation on main menu

---
+++ Scripts/SceneNavigator.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneNavigator : MonoBehaviour {
+
+	private string queuedLevel = "MainMenu";
+
+	public void QueueLevel(string level){
+
+		this.queuedLevel = level;
+
+	}
+
+	public void Load(){
+
+		SceneManager.LoadScene(queuedLevel);
+
+	}
+}
